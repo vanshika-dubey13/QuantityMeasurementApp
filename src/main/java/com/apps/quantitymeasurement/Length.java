4ca6c9ce7cb5a1468eac9d6be90506713d9f0dfe
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-02-23 12:16:05 +0530

    [Vanshika] : Extended Length class with YARDS and CENTIMETERS units

--- src/main/java/com/apps/quantitymeasurement/Length.java
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -4,12 +4,15 @@ public class Length {
 
 	private final double value;
 	private final LengthUnit unit;
+	private static final double EPSILON =  1e-3;
 
 	// Enum to represent supported units of length
 	public enum LengthUnit {
 
 		FEET(12.0), // Conversion factor: 1 Foot = 12 Inches
-		INCHES(1.0); // Conversion factor: 1 Inch = 1 Inch (base unit)
+		INCHES(1.0), // Conversion factor: 1 Inch = 1 Inch (base unit)
+		YARDS(36.0), // Conversion factor: 1 Yard = 36 Inches
+		CENTIMETERS(0.393701); // Conversion factor: 1 cm = 0.393701 Inches
 
 		private final double conversionFactor;
 
@@ -52,7 +55,16 @@ public class Length {
 		}
 
 		Length that = (Length) o;
-		return Double.compare(this.convertToBaseUnit(), that.convertToBaseUnit()) == 0;
+		double thisValue = this.convertToBaseUnit();
+		double thatValue = that.convertToBaseUnit();
+		return Math.abs(thisValue - thatValue) < EPSILON;
+	}
+	
+	@Override
+	public int hashCode() {
+	    long normalized =
+	        Math.round(convertToBaseUnit() / EPSILON);
+	    return Long.hashCode(normalized);
 	}
 
 	// Main method for standalone testing
@@ -60,5 +72,13 @@ public class Length {
 		Length length1 = new Length(1.0, LengthUnit.FEET);
 		Length length2 = new Length(12.0, LengthUnit.INCHES);
 		System.out.println("Are lengths equal? " + length1.equals(length2)); // true
+
+		Length length3 = new Length(1.0, LengthUnit.YARDS);
+		Length length4 = new Length(36.0, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length3.equals(length4)); // true
+
+		Length length5 = new Length(100.0, LengthUnit.CENTIMETERS);
+		Length length6 = new Length(39.3701, LengthUnit.INCHES);
+		System.out.println("Are lengths equal? " + length5.equals(length6)); // true
 	}
 }
