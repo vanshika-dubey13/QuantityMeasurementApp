e527503d2d330fc03a93856f9aa86a8e0a196b4c
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-02-23 12:32:50 +0530

    [Vanshika] : Extended QuantityMeasurementApp with overloaded demonstrateLengthConversion methods and illustrative examples

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -13,10 +13,24 @@ public class QuantityMeasurementApp {
 		Length l1 = new Length(value1, unit1);
 		Length l2 = new Length(value2, unit2);
 		boolean result = l1.equals(l2);
-		System.out.println("Are lengths equal? " + result);
+		System.out.println(l1 + " == " + l2 + " ? " + result);
 		return result;
 	}
 
+	public static Length demonstrateLengthConversion(double value, Length.LengthUnit fromUnit, 
+																   Length.LengthUnit toUnit) { 
+		Length source = new Length(value, fromUnit); 
+		Length converted = source.convertTo(toUnit); 
+		System.out.println(source + " -> " + converted); 
+		return converted; 
+	}
+	
+	public static Length demonstrateLengthConversion(Length length, Length.LengthUnit toUnit) { 
+		Length converted = length.convertTo(toUnit); 
+		System.out.println(length + " -> " + converted); 
+		return converted; 
+	}
+	
 	// Main method
 	public static void main(String[] args) {
 		demonstrateLengthComparison(1.0, Length.LengthUnit.FEET, 12.0, Length.LengthUnit.INCHES);
@@ -24,5 +38,12 @@ public class QuantityMeasurementApp {
 		demonstrateLengthComparison(100.0, Length.LengthUnit.CENTIMETERS, 39.3701, Length.LengthUnit.INCHES);
 		demonstrateLengthComparison(3.0, Length.LengthUnit.FEET, 1.0, Length.LengthUnit.YARDS);
 		demonstrateLengthComparison(30.48, Length.LengthUnit.CENTIMETERS, 1.0, Length.LengthUnit.FEET);
+		
+		demonstrateLengthConversion(1.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES); 
+		demonstrateLengthConversion(3.0, Length.LengthUnit.YARDS, Length.LengthUnit.FEET); 
+		demonstrateLengthConversion(36.0, Length.LengthUnit.INCHES, Length.LengthUnit.YARDS); 
+		demonstrateLengthConversion(30.48, Length.LengthUnit.CENTIMETERS, Length.LengthUnit.FEET); 
+		
+		demonstrateLengthConversion(new Length(-1.0, Length.LengthUnit.FEET), Length.LengthUnit.INCHES);
 	}
 }
