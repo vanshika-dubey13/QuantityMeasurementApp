33a678583ab93a8c3ca0eb2979209cb52dd8b6ae
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-02-23 11:59:20 +0530

    [Vanshika] : Extended QuantityMeasurement with Inches

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,7 +1,8 @@
 package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-	
+
+	// Inner class to represent Feet measurement
 	public static class Feet {
 		private final double value;
 
@@ -24,10 +25,46 @@ public class QuantityMeasurementApp {
 		}
 	}
 
-	public static void main(String[] args) {
+	// Inner class to represent Inches measurement
+	public static class Inches {
+		private final double value;
+
+		public Inches(double value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+
+			if (obj == null || this.getClass() != obj.getClass()) {
+				return false;
+			}
+
+			Inches other = (Inches) obj;
+			return Double.compare(this.value, other.value) == 0;
+		}
+	}
+
+	// Static method to demonstrate Feet equality
+	public static void demonstrateFeetEquality() {
 		Feet feet1 = new Feet(1.0);
 		Feet feet2 = new Feet(1.0);
+		System.out.println("Feet equality: " + feet1.equals(feet2));
+	}
 
-		System.out.println("Are they equal? " + feet1.equals(feet2));
+	// Static method to demonstrate Inches equality
+	public static void demonstrateInchesEquality() {
+		Inches inch1 = new Inches(1.0);
+		Inches inch2 = new Inches(1.0);
+		System.out.println("Inches equality: " + inch1.equals(inch2));
+	}
+
+	// Main method
+	public static void main(String[] args) {
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
 	}
 }
