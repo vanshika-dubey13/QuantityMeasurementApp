19d1513e90e5bab9f1b2193f8daa5567443256a5
Vanshika Dubey <dubeyvanshika021@gmail.com>
2026-02-24 14:07:17 +0530

    [Vanshika] : added Architectural JUnit test file

---
+++ src/test/java/com/apps/quantitymeasurement/ArchitecturalTest.java
@@ -0,0 +1,26 @@
+package com.apps.quantitymeasurement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArchitecturalTest {
+
+	@Test
+	public void testArchitecturalScalability_MultipleCategories() {
+		assertDoesNotThrow(() -> LengthUnit.valueOf("FEET"));
+		assertDoesNotThrow(() -> WeightUnit.valueOf("KILOGRAM"));
+	}
+
+	@Test
+	public void testUnitImmutability() {
+		assertTrue(LengthUnit.FEET instanceof Enum);
+		assertTrue(LengthUnit.INCHES instanceof Enum);
+		assertTrue(LengthUnit.YARDS instanceof Enum);
+		assertTrue(LengthUnit.CENTIMETERS instanceof Enum);
+
+		assertTrue(WeightUnit.KILOGRAM instanceof Enum);
+		assertTrue(WeightUnit.GRAM instanceof Enum);
+		assertTrue(WeightUnit.POUND instanceof Enum);
+	}
+}
